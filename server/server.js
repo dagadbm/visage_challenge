fae141a866cb418f2979fa985ea26147d1d28bc1
David Barbosa Maia <dagadbm@Davids-Air.Home>
2021-09-11 12:51:27 +0100

    feat: add cors for browser requests

--- server/server.js
+++ server/server.js
@@ -3,7 +3,14 @@ const instance = require('./db/instance');
 const routes = require('./routes');
 
 fastify.decorate('db', instance);
+
 fastify.register(routes);
+fastify.register(require('fastify-cors'), {
+  origin: (origin, cb) => {
+    cb(null, true);
+    return;
+  }
+})
 
 fastify.get("/", async () => {
   return {
@@ -13,7 +20,7 @@ fastify.get("/", async () => {
 
 const start = async () => {
   try {
-    await fastify.listen(process.env.PORT || 3000, '0.0.0.0');
+    await fastify.listen(process.env.PORT || 4000, '0.0.0.0');
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
