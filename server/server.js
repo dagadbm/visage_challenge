9a3c1d93e8918bae6e81ba102473df8401c53b35
David Barbosa Maia <dagadbm@Davids-Air.Home>
2021-09-13 17:43:24 +0100

    feat: heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const fastify = require('fastify');
 const instance = require('./db/instance');
 const routes = require('./routes');
+const path = require('path');
 
 const server = fastify({
   // 10 MB limit for the base64 pdfs (horrible solution)
   // this should be in S3 with ids and the ids on the candidates table
   bodyLimit: 10485760,
 });
+
 server.decorate('db', instance);
 
-server.register(routes);
 server.register(require('fastify-cors'), {
   origin: (origin, cb) => {
     cb(null, true);
@@ -17,11 +18,13 @@ server.register(require('fastify-cors'), {
   }
 })
 
-server.get("/", async () => {
-  return {
-    Message: "Fastify is On Fire"
-  }
+server.register(require('fastify-static'), {
+  root: path.join(__dirname, '/../client/dist'),
+});
+server.get('/', function (req, reply) {
+  return reply.sendFile('index.html')
 })
+server.register(routes);
 
 const start = async () => {
   try {
